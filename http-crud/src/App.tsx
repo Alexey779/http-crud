0c4b211b3680adc32916434d5ea28aa4b76890e3
Alexsey779 <fktrctq321@gmail.com>
2023-12-18 15:31:26 +0700

    first commit

---
+++ http-crud/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+import { Creator } from './components/Creator';
+
+function App() {
+  return (
+    <div className="App">
+      <Creator />      
+    </div>
+  );
+}
+
+export default App;
